909da93dbea3732a5d1f841ef93ab55b87e51d15
mfs98 <mfs98@ua.pt>
2018-11-21 20:43:50 +0000

    yoyo autenticação + drawer todo feito

--- NatureSpotv1.0/NatureSpot/app/src/main/java/ua/pt/naturespot/EditProfile.java
+++ NatureSpotv1.0/NatureSpot/app/src/main/java/ua/pt/naturespot/EditProfile.java
@@ -1,117 +1,102 @@
 package ua.pt.naturespot;
 
-import android.content.Intent;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
-public class EditProfile extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
 
-    DrawerLayout drawer;
-    NavigationView navigationView;
-    Toolbar toolbar=null;
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link EditProfile.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link EditProfile#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EditProfile extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public EditProfile() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment EditProfile.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EditProfile newInstance(String param1, String param2) {
+        EditProfile fragment = new EditProfile();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editprofile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        //We dont need this.
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onAttach(Context context) {
+        super.onAttach(context);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        //here is the main place where we need to work on.
-        int id=item.getItemId();
-        switch (id){
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
 
-            case R.id.nav_home:
-                Intent h= new Intent(EditProfile.this,Home.class);
-                startActivity(h);
-                break;
-            case R.id.nav_explore:
-                Intent i= new Intent(EditProfile.this,Explore.class);
-                startActivity(i);
-                break;
-            case R.id.nav_editprofile:
-                Intent t= new Intent(EditProfile.this,EditProfile.class);
-                startActivity(t);
-                break;
-            case R.id.nav_exit:
-                Intent s= new Intent(EditProfile.this,LoginActivity.class);
-                startActivity(s);
-                break;
-        }
-
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 }
--- NatureSpotv1.0/NatureSpot/app/src/main/java/ua/pt/naturespot/Home.java
+++ NatureSpotv1.0/NatureSpot/app/src/main/java/ua/pt/naturespot/Home.java
@@ -1,10 +1,12 @@
 package ua.pt.naturespot;
 
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -27,6 +30,8 @@ public class Home extends AppCompatActivity
     private FirebaseUser mFirebaseUser;
     private String mUsername;
     private String mPhotoUrl;
+
+    private MenuItem prevItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,24 +74,19 @@ public class Home extends AppCompatActivity
             }
         }
 
-
-    }
-/*
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.sign_out_menu:
-                mFirebaseAuth.signOut();
-                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
-                mUsername = ANONYMOUS;
-                startActivity(new Intent(this, SignInActivity.class));
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        Fragment myFragment = null;
+        Class fragmentClass = null;
+        fragmentClass = MyObservations.class;
+        try {
+            myFragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e){
+            e.printStackTrace();
         }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flcontent, myFragment).commit();
     }
-*/
+
 
     @Override
     public void onBackPressed() {
@@ -125,31 +125,51 @@ public class Home extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         //here is the main place where we need to work on.
+        Fragment myFragment = null;
+        Class fragmentClass = null;
         int id=item.getItemId();
         switch (id){
 
             case R.id.nav_home:
-                Intent h= new Intent(Home.this,Home.class);
+                /*Intent h= new Intent(Home.this,Home.class);
                 startActivity(h);
+                break;*/
+                fragmentClass = MyObservations.class;
                 break;
             case R.id.nav_explore:
-                Intent i= new Intent(Home.this,Explore.class);
-                startActivity(i);
+                fragmentClass = Explore.class;
                 break;
             case R.id.nav_editprofile:
-                Intent t= new Intent(Home.this,EditProfile.class);
-                startActivity(t);
+                fragmentClass = EditProfile.class;
                 break;
             case R.id.nav_exit:
-                Intent s= new Intent(Home.this,LoginActivity.class);
-                startActivity(s);
+                mFirebaseAuth.signOut();
+                Intent x= new Intent(Home.this,LoginActivity.class);
+                startActivity(x);
                 break;
+            default:
+                fragmentClass = MyObservations.class;
         }
 
+        if (fragmentClass!=null) {
+            try {
+                myFragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
 
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flcontent, myFragment).commit();
+            setTitle(item.getTitle());
+            item.setChecked(true);
+            if(prevItem!=null)
+                prevItem.setChecked(false);
+            drawer.closeDrawers();
+            prevItem=item;
+        }
+        //DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 }
+
